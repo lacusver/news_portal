586a53d27b7a05d1328f31ff111d3f6479d95f12
lacusver <39437963+lacusver@users.noreply.github.com>
2022-09-30 17:03:23 +0500

    add static root

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -175,3 +176,4 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
